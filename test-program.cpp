aafde1f8ce9dbf6a3d92770a18b038f4d7065499
Ben Hertzler <bwhertzler@outlook.com>
2024-10-30 02:27:14 -0400

    added program files: test-program.cpp

---
+++ test-program.cpp
@@ -0,0 +1,12 @@
+// Benjamin Hertzler
+// test-program.cpp, test program for dayType.h: this program contains the main() function.
+
+#include "dayType.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+}
