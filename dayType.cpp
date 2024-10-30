7aa2014768cbaa0aaa9cfc561e356f4b5d6576fb
Ben Hertzler <bwhertzler@outlook.com>
2024-10-30 02:20:24 -0400

    initial commit

--- dayType.cpp
+++ dayType.cpp
@@ -1,1 +1,121 @@
+// Benjamin Hertzler
+// dayType.cpp, class implementation for dayType.h
+
 #include "dayType.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+// 
+void dayType::setDay(string day_name)
+{
+	string day_prefix = formatDay(day_name);
+	string week_prefix;
+	currentDay = -1;
+	for (int i = 0; i < 7;)
+	{
+		week_prefix = formatDay(daysOfWeek[i]);
+		if (week_prefix.find(day_prefix) != string::npos)
+		{
+			currentDay = i;
+			break;
+		}
+	}
+}
+
+void dayType::printDay() const
+{
+	if (Invariant)
+	{
+		string today = daysOfWeek[currentDay];
+		cout << "Today is " << today << "." << endl;
+	}
+	else
+	{
+		cout << err << endl;
+	}
+
+}
+
+string dayType::getDay() const
+{
+	if (Invariant)
+	{
+		string today = daysOfWeek[currentDay];
+		return today;
+	}
+	else
+		return err;
+}
+
+string dayType::nextDay() const
+{
+	if (Invariant)
+	{
+		int tomorrow_index = cycleWeek(1);
+		string tomorrow = daysOfWeek[tomorrow_index];
+		return tomorrow;
+	}
+	else
+		return err;
+}
+
+string dayType::previousDay() const
+{
+	if (Invariant)
+	{
+		int yesterday_index = cycleWeek(-1);
+		string yesterday = daysOfWeek[yesterday_index];
+		return yesterday;
+	}
+	else
+		return err;
+}
+
+string dayType::addDays(int days) const
+{
+	if (Invariant)
+	{
+		int day_index = cycleWeek(days);
+		string new_day = daysOfWeek[day_index];
+		return new_day;
+	}
+	else
+		return err;
+}
+
+// Constructors
+
+dayType::dayType(string day_name = "")
+{
+	setDay(day_name);
+}
+
+// Private Functions
+
+int dayType::cycleWeek(int cycle_days) const
+{
+	int cycle_index = currentDay + cycle_days;
+	int day_index = cycle_index % 7;
+	return day_index;
+}
+
+string dayType::formatDay(string day)
+{
+	string formatted = day.substr(0, 2);
+	for (int i = 0; i < 3; i++)
+		formatted[i] = (char)tolower(i);
+
+	return formatted;
+}
+
+// Class Invariant
+
+bool dayType::Invariant() const
+{
+	if (currentDay <= 6 && currentDay >= 0)
+		return true;
+	else
+		return false;
+}
--- dayType.h
+++ dayType.h
@@ -1,5 +1,60 @@
+// Benjamin Hertzler
+// dayType.h, the specification file for the class dayType
+
 #pragma once
+
+#include <iostream>
+#include <string>
+using namespace std;
+
 class dayType
 {
+public:
+	void setDay(string day_name);
+	// Function to set the day.
+	// The day should be a string containing at least the first three letters of one of the days of the week.
+	// Postcondition: The function will set the value of current day to -1 if the input is invalid.
+
+	void printDay() const;
+	// Function to output the day name corresponding to currentDay to the console.
+	// Precondition: The function will output an error message if the value of currentDay is not between 0 and 6.
+	// Postcondition: The output is formatted as the sentence "Today is [currentDay]."
+
+	string getDay() const;
+	// Function to return the name of currentDay.
+	// Precondition: The string returned will be an error message if the value of currentDay is not between 0 and 6.
+	// Postcondition: The value returned will be the string in daysOfWeek at index currentDay.
+
+	string nextDay() const;
+	// Function to return the name of the following day.
+	// Precondition: The string returned will be an error message if the value of currentDay is not between 0 and 6.
+	// Postcondition: The value returned will be the name of the week day immediately after currentDay.
+
+	string previousDay() const;
+	// Function to return the name of the previous day.
+	// Precondition: The string returned will be an error message if the value of currentDay is not between 0 and 6.
+	// Postcondition: The value returned will be the name of the week day immediately before currentDay.
+
+	string addDays(int days) const;
+	// Function to increment the day by a specified number of days.
+	// Precondition: The string returned will be an error message if the value of currentDay is not between 0 and 6.
+	// Postcondition: The value returned will be the name of the week day of the input number of days after currentDay.
+	
+	dayType(string day_name = "");
+	// Constructor with optional parameters, initialized to "" by default.
+	// The day is set according to the parameter.
+	// Postconditon: currentDay = daysOfWeek[day_name].
+	//					Invalid values for day_name will set currentDay to -1.
+	//					The default parameters will set currentDay to -1.
+
+private:
+	int cycleWeek(int) const;				// Internal function to change the index of the day of the week.
+	string formatDay(string);				// Internal function to format input parameter.
+	bool Invariant() const;					// Invariant function.
+
+	int currentDay;							// Variable to store the position of the day within the week.
+	const string err = "Invalid Day Name.";	// Message displayed if the value of currentDay is out of bounds.
+	const string daysOfWeek[7] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };	// Array to store day names.
+
 };
 
